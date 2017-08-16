1c6688b7815df80de47475de09e34dc792c4b203
NetBUG <oleg@skuuper.com>
2017-08-16 19:32:43 +0300

    Now building correctly; still for Roomba

--- rbot_100_driver/rbot_100_driver/src/rbot100_lightweight.cpp
+++ rbot_100_driver/rbot_100_driver/src/rbot100_lightweight.cpp
@@ -46,7 +46,7 @@
 #include <string>
 
 std::string port;
-rbot::RBotInterface * rbot;
+rbot::RBotInterface * my_robot;
 
 
 std::string prefixTopic(std::string prefix, char * name)
@@ -59,13 +59,13 @@ std::string prefixTopic(std::string prefix, char * name)
 
 void cmdVelReceived(const geometry_msgs::Twist::ConstPtr& cmd_vel)
 {
-	rbot->drive(cmd_vel->linear.x, cmd_vel->angular.z);
+	my_robot->drive(cmd_vel->linear.x, cmd_vel->angular.z);
 }
 
 
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "rbot560_light_node");
+	ros::init(argc, argv, "rbot100_light_node");
 
 	ROS_INFO("rbot for ROS %.2f", NODE_VERSION);
 	
@@ -77,13 +77,13 @@ int main(int argc, char** argv)
 	
 	n.param<std::string>("rbot/port", port, "/dev/ttyUSB0");
 	
-	rbot = new rbot::OpenInterface(port.c_str());
+	my_robot = new rbot::RBotInterface(port.c_str());
 	
 	ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("/odom", 50);
 	tf::TransformBroadcaster odom_broadcaster;
 	ros::Subscriber cmd_vel_sub  = n.subscribe<geometry_msgs::Twist>("/cmd_vel", 1, cmdVelReceived);
 
-	if( rbot->openSerialPort(true) == 0) ROS_INFO("Connected to rbot.");
+	if( my_robot->openSerialPort(true) == 0) ROS_INFO("Connected to rbot.");
 	else
 	{
 		ROS_FATAL("Could not connect to rbot.");
@@ -112,24 +112,24 @@ int main(int argc, char** argv)
 			heartvalue = 0;
 			inc=true;
 		}
-		rbot->setLeds(0, 0, 0, 0, (unsigned char)heartvalue, 255);*/
+		my_robot->setLeds(0, 0, 0, 0, (unsigned char)heartvalue, 255);*/
 
 		current_time = ros::Time::now();
 		
-		last_x = rbot->odometry_x_;
-		last_y = rbot->odometry_y_;
-		last_yaw = rbot->odometry_yaw_;
+		last_x = my_robot->odometry_x_;
+		last_y = my_robot->odometry_y_;
+		last_yaw = my_robot->odometry_yaw_;
 		
-		if( rbot->getSensorPackets(100) == -1) ROS_ERROR("Could not retrieve sensor packets.");
-		else rbot->calculateOdometry();
+		if( my_robot->getSensorPackets(100) == -1) ROS_ERROR("Could not retrieve sensor packets.");
+		else my_robot->calculateOdometry();
 		
 		dt = (current_time - last_time).toSec();
-		vel_x = (rbot->odometry_x_ - last_x)/dt;
-		vel_y = (rbot->odometry_y_ - last_y)/dt;
-		vel_yaw = (rbot->odometry_yaw_ - last_yaw)/dt;
+		vel_x = (my_robot->odometry_x_ - last_x)/dt;
+		vel_y = (my_robot->odometry_y_ - last_y)/dt;
+		vel_yaw = (my_robot->odometry_yaw_ - last_yaw)/dt;
 		
 		//since all odometry is 6DOF we'll need a quaternion created from yaw
-		geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(rbot->odometry_yaw_);
+		geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(my_robot->odometry_yaw_);
 		
 		//first, we'll publish the transform over tf
 		geometry_msgs::TransformStamped odom_trans;
@@ -137,8 +137,8 @@ int main(int argc, char** argv)
 		odom_trans.header.frame_id = "odom";
 		odom_trans.child_frame_id = "base_link";
 		
-		odom_trans.transform.translation.x = rbot->odometry_x_;
-		odom_trans.transform.translation.y = rbot->odometry_y_;
+		odom_trans.transform.translation.x = my_robot->odometry_x_;
+		odom_trans.transform.translation.y = my_robot->odometry_y_;
 		odom_trans.transform.translation.z = 0.0;
 		odom_trans.transform.rotation = odom_quat;
 		
@@ -151,8 +151,8 @@ int main(int argc, char** argv)
 		odom.header.frame_id = "odom";
 		
 		//set the position
-		odom.pose.pose.position.x = rbot->odometry_x_;
-		odom.pose.pose.position.y = rbot->odometry_y_;
+		odom.pose.pose.position.x = my_robot->odometry_x_;
+		odom.pose.pose.position.y = my_robot->odometry_y_;
 		odom.pose.pose.position.z = 0.0;
 		odom.pose.pose.orientation = odom_quat;
 		
@@ -169,8 +169,8 @@ int main(int argc, char** argv)
 		r.sleep();
 	}
 	
-	rbot->powerDown();
-	rbot->closeSerialPort();
+	my_robot->powerDown();
+	my_robot->closeSerialPort();
 }
 
 // EOF
